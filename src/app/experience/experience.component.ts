6147b42501ebcbfb5d6c713abe0386497a754866
David Lopez Chavez <dlsun18@hotmail.com>
2024-06-02 14:31:15 -0600

    actualizacion

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MainComponent } from "../main/main.component";
 
 @Component({
-  selector: 'app-experience',
-  standalone: true,
-  imports: [],
-  templateUrl: './experience.component.html',
-  styleUrl: './experience.component.css'
+    selector: 'app-experience',
+    standalone: true,
+    templateUrl: './experience.component.html',
+    styleUrl: './experience.component.css',
+    imports: [MainComponent]
 })
 export class ExperienceComponent {
 
