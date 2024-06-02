37b28bad164027b0ff87379fe23b52f744053015
David Lopez Chavez <dlsun18@hotmail.com>
2024-06-02 14:47:09 -0600

    ac

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { ExperienceComponent } from "../experience/experience.component";
+import { SkillsComponent } from "../skills/skills.component";
+import { ProjectsComponent } from "../projects/projects.component";
 
 @Component({
-  selector: 'app-main',
-  standalone: true,
-  imports: [],
-  templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+    selector: 'app-main',
+    standalone: true,
+    templateUrl: './main.component.html',
+    styleUrl: './main.component.css',
+    imports: [ExperienceComponent, SkillsComponent, ProjectsComponent]
 })
 export class MainComponent {
 
