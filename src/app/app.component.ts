4458e447b25ee8438bb389de6bd5f449fd36fb3b
David Lopez Chavez <dlsun18@hotmail.com>
2024-06-02 16:37:46 -0600

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainComponent } from "./main/main.component";
+import { ExperienceComponent } from "./experience/experience.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet]
+    imports: [RouterOutlet, MainComponent, ExperienceComponent, SkillsComponent, ProjectsComponent, ContactComponent]
 })
 export class AppComponent {
   title = 'Portfolio-CV';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'main',component: MainComponent},
+    {path:'experience',component: ExperienceComponent},
+    {path:'skills',component: SkillsComponent},
+    {path:'projects',component: ProjectsComponent},
+    {path:'contact',component: ContactComponent},
+];
